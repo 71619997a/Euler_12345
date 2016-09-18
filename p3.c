9dbb0e934dafd964084cfeb28600dc8293839a43
71619997a <gmarks@stuy.edu>
2016-09-18 16:10:15 -0400

    Finish p3

--- p3.c
+++ p3.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-	long long num = 600851475143;
-	
+	long long num = 600851475143;  // just in case
+	int div = 1;
+	while(num > 1) {
+		div++;
+		if(isPrime(div)) {
+			while(num % div == 0) {
+				num /= div;
+			}
+		}
+	}
+	printf("%d", div);
+}
+
+int isPrime(int num) {
+	int i = 2;
+	int sqrtnum = (int)sqrt(num);
+	for(;i <= sqrtnum; i++) {
+		if(num % i == 0) return 0;
+	}
+	return 1;
 }
